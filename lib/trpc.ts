bfe7dca8b43ac8c5ed93d8e42e7363cf0201aad8
Vap0r1ze <superdash993@gmail.com>
2025-07-02 00:26:46 -0400

    debugging

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -1,6 +1,6 @@
 import type { AppRouter } from '@/server/trpc/router';
 import { getBaseUrl, isClient } from '@/lib/utils';
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink, loggerLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import { ssrPrepass } from '@trpc/next/ssrPrepass';
 import { inferRouterOutputs } from '@trpc/server';
@@ -9,6 +9,13 @@ export const trpc = createTRPCNext<AppRouter>({
     config({ ctx }) {
         return {
             links: [
+                loggerLink({
+                    enabled: (opts) => {
+                        if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') return true;
+                        if (opts.direction === 'down' && opts.result instanceof Error) return true;
+                        return false;
+                    },
+                }),
                 httpBatchLink({
                     url: `${getBaseUrl()}/api/trpc`,
                     headers: isClient ? void 0 : () => {
